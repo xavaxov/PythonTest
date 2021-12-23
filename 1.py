9b438df83e15bb59b3492798abedc49755564dda
XavaXov <xavaxov@gmail.com>
2021-12-23 10:04:35 +0300

    1

--- 1.py
+++ 1.py
@@ -1,35 +1,43 @@
-from random import randrange
+import random
 
 n = 15 # количество строк
 m = 15 # количество столбцов
-a = [[0] * m for i in range(n)]
+
+a = [[random.choice([0,0,0,0,0,'■']) for j in range(m)] for i in range(n)]
 b = [['.'] * m for i in range(n)]
-start = [0, 2] 
-finish = [14,10]
-rocks = [[1, 2],[1, 4],[1, 5],[2, 1],[3,3]]
+for i in range(n): 
+    for j in range(m):
+        if a[i][j] == '■':
+            b[i][j] = '■'
+# a = [[0] * m for i in range(n)]
+# b = [['.'] * m for i in range(n)]
+start = [random.randint(0,n-1), random.randint(0,m-1)] 
+finish = [random.randint(0,n-1), random.randint(0,m-1)]
+# rocks = [[1, 2],[1, 4],[1, 5],[2, 1],[3,3]]
 path = []
 
 
 def PrintArr(arr): # функция вывода массива
     for i in range(len(arr)): 
         for j in range(len(arr[i])):
-            print(arr[i][j], end=' ')
+            print(arr[i][j], end=' ') if len(str(arr[i][j])) == 2 else print(' ' + str(arr[i][j]), end=' ')
         print()
 
 d = 1
 flag = 0
 a[start[0]][start[1]] = d # помечаем стартоввую ячейку
-def RocksArr(a, rocks):
-    for i in range(len(rocks)):
-        a[rocks[i][0]][rocks[i][1]] = '@'
+a[finish[0]][finish[1]] = 0
 
-RocksArr(a, rocks)
-RocksArr(b, rocks)
+# def RocksArr(a, rocks):
+#    for i in range(len(rocks)):
+#        a[rocks[i][0]][rocks[i][1]] = '@'
+# RocksArr(a, rocks)
+# RocksArr(b, rocks)
 
 b[start[0]][start[1]] = 'S'
 b[finish[0]][finish[1]] = 'F'
-PrintArr(b)
-print('------')
+# PrintArr(b)
+# print('------')
 
 while a[finish[0]][finish[1]] == 0 and flag == 0:
     flag = 1
@@ -55,8 +63,8 @@ while a[finish[0]][finish[1]] == 0 and flag == 0:
     d = d + 1
 
 
-PrintArr(a)
-print('------')
+# PrintArr(a)
+# print('------')
 
 if flag == 0:
     cur = finish
@@ -78,13 +86,13 @@ if flag == 0:
             b[x][y] = '←'
             y = y - 1
         cur = [x, y]    
+    b[start[0]][start[1]] = 'F'
+    b[finish[0]][finish[1]] = 'S'
+    PrintArr(b)
+    print('------')
+    print(path)
 else:
+    PrintArr(b)
+    print('------')
     print('no path') 
 
-
-# for i in path:
- #   b[i[0]][i[1]] = 'x'
-
-PrintArr(b)
-print('------')
-print(path)
