f975233819d7bbc5e7a38ae2f13655ba11862c5c
XavaXov <xavaxov@gmail.com>
2021-12-28 11:24:30 +0300

    fix

--- 2.py
+++ 2.py
@@ -11,6 +11,7 @@ m = 15 # количество столбцов
 a = [[random.choice(['n','r','d','c']) for j in range(m)] for i in range(n)]
 b = [[0] * m for i in range(n)]
 c = [[''] * m for i in range(n)]
+path = []
 
 start = [random.randint(0,n-1), random.randint(0,m-1)] 
 finish = [random.randint(0,n-1), random.randint(0,m-1)]
@@ -18,17 +19,20 @@ finish = [random.randint(0,n-1), random.randint(0,m-1)]
 c[start[0]][start[1]] = 'S'
 c[finish[0]][finish[1]] = 'F'
 
-for i in range(len(a)): 
-    for j in range(len(a[i])):
-        if a[i][j] == 'n':
-            print('   ', end='') if c[i][j] == '' else  print(' ' + c[i][j] +' ', end='')
-        elif a[i][j] == 'r':
-            print('  |', end='') if c[i][j] == '' else  print(' ' + c[i][j] +'|', end='')
-        elif a[i][j] == 'd':
-            print('___', end='') if c[i][j] == '' else  print('_' + c[i][j] +'_', end='')
-        elif a[i][j] == 'c':
-            print('__|', end='') if c[i][j] == '' else  print('_' + c[i][j] +'|', end='')
-    print()
+def PrintArr(a, c):
+    for i in range(len(a)): 
+        for j in range(len(a[i])):
+            if a[i][j] == 'n':
+                print('   ', end='') if c[i][j] == '' else  print(' ' + c[i][j] +' ', end='')
+            elif a[i][j] == 'r':
+                print('  |', end='') if c[i][j] == '' else  print(' ' + c[i][j] +'|', end='')
+            elif a[i][j] == 'd':
+                print('___', end='') if c[i][j] == '' else  print('_' + c[i][j] +'_', end='')
+            elif a[i][j] == 'c':
+                print('__|', end='') if c[i][j] == '' else  print('_' + c[i][j] +'|', end='')
+        print()
+
+PrintArr(a, c)
 print('-----')
 
 d = 1
@@ -59,3 +63,28 @@ for i in range(len(b)):
             print(b[i][j], end=' ') if len(str(b[i][j])) == 2 else print(' ' + str(b[i][j]), end=' ')
         print()
 print('-----')
+
+if flag == 0:
+    cur = finish
+    while cur != start:
+        x = cur[0]
+        y = cur[1]
+        path.append(cur)
+        if x+1<n and x+1>-1 and b[x+1][y] == b[x][y] - 1:
+            c[x][y] = '↓'
+            x = x + 1
+        elif x-1<n and x-1>-1 and b[x-1][y] == b[x][y] - 1:
+            c[x][y] = '↑'
+            x = x - 1
+        elif y+1<m and y+1>-1 and b[x][y+1] == b[x][y] - 1:
+            c[x][y] = '→'
+            y = y + 1
+        elif y-1<m and y-1>-1 and b[x][y-1] == b[x][y] - 1:
+            c[x][y] = '←'
+            y = y - 1
+        cur = [x, y]   
+    c[start[0]][start[1]] = 'F'
+    c[finish[0]][finish[1]] = 'S'
+    PrintArr(a, c)
+else:
+    print('no path') 
